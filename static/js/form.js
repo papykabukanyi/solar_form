06e2c748d9d164ffcabb149365812697ceb69c79
Papi <papykabukanyi@gmail.com>
2024-05-04 14:37:48 -0700

    added favicons, update titles, descriptions. modify form intake

--- static/js/form.js
+++ static/js/form.js
@@ -1,39 +1,19 @@
-const stepMenuOne = document.querySelector('.formbold-step-menu1')
-const stepMenuTwo = document.querySelector('.formbold-step-menu2')
-const stepMenuThree = document.querySelector('.formbold-step-menu3')
-const stepOne = document.querySelector('.formbold-form-step-1')
-const stepTwo = document.querySelector('.formbold-form-step-2')
-const stepThree = document.querySelector('.formbold-form-step-3')
-const formSubmitBtn = document.querySelector('.formbold-btn')
-const formBackBtn = document.querySelector('.formbold-back-btn')
-formSubmitBtn.addEventListener("click", function(event){
-    event.preventDefault()
-    if(stepMenuOne.className == 'formbold-step-menu1 active') {
-        event.preventDefault()
-        stepMenuOne.classList.remove('active')
-        stepMenuTwo.classList.add('active')
-        stepOne.classList.remove('active')
-        stepTwo.classList.add('active')
-        formBackBtn.classList.add('active')
-        formBackBtn.addEventListener("click", function (event) {
-            event.preventDefault()
-            stepMenuOne.classList.add('active')
-            stepMenuTwo.classList.remove('active')
-            stepOne.classList.add('active')
-            stepTwo.classList.remove('active')
-            formBackBtn.classList.remove('active')
+$(document).ready(function(){
+  $(".form-wrapper .button").click(function(){
+    var button = $(this);
+    var currentSection = button.parents(".section");
+    var currentSectionIndex = currentSection.index();
+    var headerSection = $('.steps li').eq(currentSectionIndex);
+    currentSection.removeClass("is-active").next().addClass("is-active");
+    headerSection.removeClass("is-active").next().addClass("is-active");
 
-            })
-        } else if(stepMenuTwo.className == 'formbold-step-menu2 active') {
-        event.preventDefault()
-        stepMenuTwo.classList.remove('active')
-        stepMenuThree.classList.add('active')
-        stepTwo.classList.remove('active')
-        stepThree.classList.add('active')
-        formBackBtn.classList.remove('active')
-        formSubmitBtn.textContent = 'Submit'
-        } else if(stepMenuThree.className == 'formbold-step-menu3 active') {
+    $(".form-wrapper").submit(function(e) {
+      e.preventDefault();
+    });
 
-            document.querySelector('form').submit()
-        }
-    })
+    if(currentSectionIndex === 3){
+      $(document).find(".form-wrapper .section").first().addClass("is-active");
+      $(document).find(".steps li").first().addClass("is-active");
+    }
+  });
+});
